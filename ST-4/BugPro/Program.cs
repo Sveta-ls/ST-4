34479f7ea940fc912e8772f6f6cd89ace07b9d7b
Sveta-ls <slana6570@gmail.com>
2026-04-13 18:56:45 +0300

    lab 4

---
+++ ST-4/BugPro/Program.cs
@@ -0,0 +1,155 @@
+﻿using Stateless;
+
+namespace BugPro;
+
+public enum BugState
+{
+    Open,
+    Assigned,
+    InProgress,
+    Resolved,
+    Verified,
+    Closed,
+    Reopened,
+    Deferred,
+    Duplicate,
+    WonTFix,
+    Invalid
+}
+
+public enum BugTrigger
+{
+    Assign,
+    StartProgress,
+    Resolve,
+    Verify,
+    Close,
+    Reopen,
+    Defer,
+    MarkDuplicate,
+    MarkWonTFix,
+    MarkInvalid,
+    Reassign,
+    FailVerification
+}
+
+public class Bug
+{
+    private readonly StateMachine<BugState, BugTrigger> _machine;
+    private string _currentAssignee = string.Empty;
+
+    public BugState CurrentState => _machine.State;
+    public string CurrentAssignee => _currentAssignee;
+
+    public Bug(string description)
+    {
+        _machine = new StateMachine<BugState, BugTrigger>(BugState.Open);
+        ConfigureTransitions();
+    }
+
+    private void ConfigureTransitions()
+    {
+        _machine.Configure(BugState.Open)
+            .Permit(BugTrigger.Assign, BugState.Assigned)
+            .Permit(BugTrigger.Defer, BugState.Deferred)
+            .Permit(BugTrigger.MarkDuplicate, BugState.Duplicate)
+            .Permit(BugTrigger.MarkWonTFix, BugState.WonTFix)
+            .Permit(BugTrigger.MarkInvalid, BugState.Invalid);
+
+        _machine.Configure(BugState.Assigned)
+            .Permit(BugTrigger.StartProgress, BugState.InProgress)
+            .Permit(BugTrigger.Defer, BugState.Deferred)
+            .PermitReentry(BugTrigger.Reassign);
+
+        _machine.Configure(BugState.InProgress)
+            .Permit(BugTrigger.Resolve, BugState.Resolved)
+            .Permit(BugTrigger.Reassign, BugState.Assigned);
+
+        _machine.Configure(BugState.Resolved)
+            .Permit(BugTrigger.Verify, BugState.Verified)
+            .Permit(BugTrigger.FailVerification, BugState.Reopened)
+            .Permit(BugTrigger.Reopen, BugState.Reopened);
+
+        _machine.Configure(BugState.Verified)
+            .Permit(BugTrigger.Close, BugState.Closed)
+            .Permit(BugTrigger.Reopen, BugState.Reopened);
+
+        _machine.Configure(BugState.Closed)
+            .Ignore(BugTrigger.Reopen);
+
+        _machine.Configure(BugState.Reopened)
+            .Permit(BugTrigger.Assign, BugState.Assigned)
+            .Permit(BugTrigger.StartProgress, BugState.InProgress);
+
+        _machine.Configure(BugState.Deferred)
+            .Permit(BugTrigger.Assign, BugState.Assigned)
+            .Permit(BugTrigger.Reopen, BugState.Reopened);
+
+        _machine.Configure(BugState.Duplicate)
+            .Ignore(BugTrigger.Reopen);
+
+        _machine.Configure(BugState.WonTFix)
+            .Ignore(BugTrigger.Reopen);
+
+        _machine.Configure(BugState.Invalid)
+            .Ignore(BugTrigger.Reopen);
+    }
+
+    public void Assign(string assignee)
+    {
+        _currentAssignee = assignee;
+        _machine.Fire(BugTrigger.Assign);
+    }
+
+    public void StartProgress() => _machine.Fire(BugTrigger.StartProgress);
+    public void Resolve() => _machine.Fire(BugTrigger.Resolve);
+    public void Verify() => _machine.Fire(BugTrigger.Verify);
+    public void Close() => _machine.Fire(BugTrigger.Close);
+    public void Reopen() => _machine.Fire(BugTrigger.Reopen);
+    public void Defer() => _machine.Fire(BugTrigger.Defer);
+    public void MarkDuplicate() => _machine.Fire(BugTrigger.MarkDuplicate);
+    public void MarkWonTFix() => _machine.Fire(BugTrigger.MarkWonTFix);
+    public void MarkInvalid() => _machine.Fire(BugTrigger.MarkInvalid);
+    
+    public void Reassign(string newAssignee)
+    {
+        _currentAssignee = newAssignee;
+        _machine.Fire(BugTrigger.Reassign);
+    }
+    
+    public void FailVerification() => _machine.Fire(BugTrigger.FailVerification);
+}
+
+class Program
+{
+    static void Main()
+    {
+        var bug = new Bug("1 бага");
+       
+        bug.Assign("Lady Gaga");
+        bug.StartProgress();
+        bug.Resolve();
+        bug.Verify();
+        Console.WriteLine($"состояние: {bug.CurrentState}");
+
+        bug.Close();
+        var bug2 = new Bug("2 баг");
+        Console.WriteLine($"новый баг: {bug2.CurrentState}");
+        
+        bug2.MarkDuplicate();
+        Console.WriteLine($"баг отмечен как дубликат: {bug2.CurrentState}");
+        
+        var bug3 = new Bug("3 баг");
+        bug3.MarkInvalid();
+        Console.WriteLine($"баг отмечен как невалидный: {bug3.CurrentState}");
+        
+        var bug4 = new Bug("4 баг");
+        bug4.Defer();
+        bug4.Assign("Sveta Zyazeva");
+        bug4.StartProgress();
+        bug4.Resolve();
+        bug4.Verify();
+        bug4.Close();
+        Console.WriteLine($"баг закрыт: {bug4.CurrentState}");
+    }
+}
---
+++ ST-4/BugTests/UnitTest1.cs
@@ -0,0 +1,224 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BugPro;
+
+namespace BugTests;
+
+[TestClass]
+public class BugWorkflowTests
+{
+    [TestMethod]
+    public void Test_NewBug_ShouldBeInOpenState()
+    {
+        var bug = new Bug("Test bug");
+        Assert.AreEqual(BugState.Open, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_AssignBug_ShouldTransitionToAssigned()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        Assert.AreEqual(BugState.Assigned, bug.CurrentState);
+        Assert.AreEqual("Developer1", bug.CurrentAssignee);
+    }
+
+    [TestMethod]
+    public void Test_StartProgress_ShouldTransitionToInProgress()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.StartProgress();
+        Assert.AreEqual(BugState.InProgress, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_ResolveBug_ShouldTransitionToResolved()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.StartProgress();
+        bug.Resolve();
+        Assert.AreEqual(BugState.Resolved, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_VerifyBug_ShouldTransitionToVerified()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.StartProgress();
+        bug.Resolve();
+        bug.Verify();
+        Assert.AreEqual(BugState.Verified, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_CloseBug_ShouldTransitionToClosed()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.StartProgress();
+        bug.Resolve();
+        bug.Verify();
+        bug.Close();
+        Assert.AreEqual(BugState.Closed, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_ReopenFromResolved_ShouldTransitionToReopened()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.StartProgress();
+        bug.Resolve();
+        bug.Reopen();
+        Assert.AreEqual(BugState.Reopened, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_DeferBug_ShouldTransitionToDeferred()
+    {
+        var bug = new Bug("Test bug");
+        bug.Defer();
+        Assert.AreEqual(BugState.Deferred, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_MarkDuplicate_ShouldTransitionToDuplicate()
+    {
+        var bug = new Bug("Test bug");
+        bug.MarkDuplicate();
+        Assert.AreEqual(BugState.Duplicate, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_MarkInvalid_ShouldTransitionToInvalid()
+    {
+        var bug = new Bug("Test bug");
+        bug.MarkInvalid();
+        Assert.AreEqual(BugState.Invalid, bug.CurrentState);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void Test_InvalidTransition_StartProgressWithoutAssign_ShouldThrowException()
+    {
+        var bug = new Bug("Test bug");
+        bug.StartProgress();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void Test_InvalidTransition_ResolveWithoutStartingProgress_ShouldThrowException()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.Resolve();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void Test_InvalidTransition_VerifyWithoutResolving_ShouldThrowException()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.StartProgress();
+        bug.Verify();
+    }
+
+    [TestMethod]
+    public void Test_FailVerification_ShouldTransitionToReopened()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.StartProgress();
+        bug.Resolve();
+        bug.FailVerification();
+        Assert.AreEqual(BugState.Reopened, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_ReassignBug_ShouldStayInAssignedState()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.Reassign("Developer2");
+        Assert.AreEqual(BugState.Assigned, bug.CurrentState);
+        Assert.AreEqual("Developer2", bug.CurrentAssignee);
+    }
+
+    [TestMethod]
+    public void Test_ClosedBug_ShouldIgnoreReopen()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.StartProgress();
+        bug.Resolve();
+        bug.Verify();
+        bug.Close();
+        bug.Reopen();
+        Assert.AreEqual(BugState.Closed, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_MarkWonTFix_ShouldTransitionToWonTFix()
+    {
+        var bug = new Bug("Test bug");
+        bug.MarkWonTFix();
+        Assert.AreEqual(BugState.WonTFix, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_DeferredBug_CanBeAssigned()
+    {
+        var bug = new Bug("Test bug");
+        bug.Defer();
+        bug.Assign("Developer1");
+        Assert.AreEqual(BugState.Assigned, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_CompleteWorkflow_HappyPath()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.StartProgress();
+        bug.Resolve();
+        bug.Verify();
+        bug.Close();
+        Assert.AreEqual(BugState.Closed, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_ReopenedBug_CanBeReassigned()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.StartProgress();
+        bug.Resolve();
+        bug.Reopen();
+        bug.Assign("Developer2");
+        Assert.AreEqual(BugState.Assigned, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_ReassignFromInProgress_ShouldTransitionToAssigned()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.StartProgress();
+        bug.Reassign("Developer2");
+        Assert.AreEqual(BugState.Assigned, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_DoubleAssignment_ShouldStayInAssignedState()
+    {
+        var bug = new Bug("Test bug");
+        bug.Assign("Developer1");
+        bug.Reassign("Developer2");
+        bug.Reassign("Developer3");
+        Assert.AreEqual(BugState.Assigned, bug.CurrentState);
+        Assert.AreEqual("Developer3", bug.CurrentAssignee);
+    }
+}
